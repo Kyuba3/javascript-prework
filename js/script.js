d0ed33941d6c7b7af30a0069e4d29de4f4623a51
Kuba Kołodziej <jmk.kolodziej@gmail.com>
2022-08-31 08:52:39 +0200

    Add new variables for js

---
+++ js/script.js
@@ -0,0 +1,6 @@
+let computerMove = 'kamień';
+let playerMove = 'papier';
+
+printMessage('Zagrałem' + computerMove + '! Jeśli Twój ruch to' + playerMove + ' to wygrywasz');
+printMessage('Mój ruch to ' + playerMove + ' więc wygrywam!');
+
